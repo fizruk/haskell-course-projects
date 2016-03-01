42c1060dee39c874eea206af2f25607329b0df04
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2016-03-01 08:22:59 +0300

    Set language to Russian

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ author = u'Nickolay Kudasov'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "ru"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
